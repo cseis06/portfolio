6f4ac5b95d07a6af85a24a1d4fc2c1c1b7e8bc3c
Daniela Brunetto <dbrunetto.informatica@centralshop.com.py>
2025-09-25 15:35:55 -0300

    desactivar reglas de ESlint

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { motion } from "framer-motion";
-import React from "react";
+'use client';
+import { motion } from 'framer-motion';
+import React from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export const BackgroundBeams = React.memo(
   ({ className }: { className?: string }) => {
